35e252d1073ae14414eb211d6c9c532d89de2f65
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-09 10:02:27 +0530

    making greet concise

--- DXC_REACT/dxcReact/src/components/Greet.js
+++ DXC_REACT/dxcReact/src/components/Greet.js
@@ -1,7 +1,10 @@
 import react from 'react'
 import App from '../App'
 
-function Greet(){
+/*function Greet(){
    return <h1>welcome shaggu</h1>
-}
+}*/
+
+   const Greet = () => <h1>hello dxc</h1>
+
 export default Greet
